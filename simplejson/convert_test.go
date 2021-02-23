0155e653b23980a7f298adb6eac0eb34c00aff01
Divjot Arora <divjot.arora@10gen.com>
2021-02-23 14:22:05 -0500

    Support tranforming JSON to Go structs

---
+++ simplejson/convert_test.go
@@ -0,0 +1,35 @@
+package simplejson
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/sindbach/json-to-bson-go/jsonutil"
+)
+
+func TestConvert(t *testing.T) {
+	testCases := []struct {
+		name      string
+		filename  string
+		expectErr bool
+	}{
+		{"scalar values", "simplejson_scalar", false},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			input := jsonutil.ReadFileBytesOrPanic(fmt.Sprintf("../examples/%s.json", tc.filename))
+			actual, err := Convert(input)
+			if tc.expectErr {
+				if err == nil {
+					t.Fatalf("expected error, got nil")
+				}
+				return
+			}
+
+			expected := jsonutil.ReadFileBytesOrPanic(fmt.Sprintf("../examples/%s.go", tc.filename))
+			if string(expected) != actual {
+				t.Fatalf("expected generated struct %s, got %s", string(expected), actual)
+			}
+		})
+	}
+}
