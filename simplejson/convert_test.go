858b73d9b9818a8f14e60d5e9636e65117f9a3f8
Divjot Arora <divjot.arora@10gen.com>
2021-02-24 13:13:59 -0500

    Support options in simplejson.Convert

--- simplejson/convert_test.go
+++ simplejson/convert_test.go
@@ -5,22 +5,41 @@ import (
 	"testing"
 
 	"github.com/sindbach/json-to-bson-go/jsonutil"
+	"github.com/sindbach/json-to-bson-go/options"
 )
 
 func TestConvert(t *testing.T) {
+	customNameOpts := options.NewOptions().SetStructName("CustomStruct")
+	minimizeTrueOpts := options.NewOptions().SetMinimizeIntegerSize(true)
+	minimizeFalseOpts := options.NewOptions().SetMinimizeIntegerSize(false)
+	truncateTrueMinFalseOpts := options.NewOptions().
+		SetTruncateIntegers(true).
+		SetMinimizeIntegerSize(false)
+	truncateTrueOpts := options.NewOptions().SetTruncateIntegers(true)
+	truncateFalseOpts := options.NewOptions().SetTruncateIntegers(false)
+
 	testCases := []struct {
 		name      string
 		filename  string
+		opts      *options.Options
 		expectErr bool
 	}{
-		{"scalar values", "simplejson_scalar", false},
-		{"arrays", "simplejson_array", false},
-		{"nested documents", "simplejson_nested", false},
+		{"scalar values", "simplejson_scalar", nil, false},
+		{"arrays", "simplejson_array", nil, false},
+		{"nested documents", "simplejson_nested", nil, false},
+		{"custom struct name", "simplejson_custom_name", customNameOpts, false},
+		// The scalar values file contains examples of numeric values being represented as types other than float64.
+		// The MinimizeIntegerSize option is true by default, so that test passes without any options as well.
+		{"minimize ints true", "simplejson_scalar", minimizeTrueOpts, false},
+		{"minimize ints false", "simplejson_minimize_false", minimizeFalseOpts, false},
+		{"truncate ints true is a noop if minimize is false", "simplejson_minimize_false", truncateTrueMinFalseOpts, false},
+		{"truncate true", "simplejson_truncate_true", truncateTrueOpts, false},
+		{"truncate false", "simplejson_scalar", truncateFalseOpts, false},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			input := jsonutil.ReadFileBytesOrPanic(fmt.Sprintf("../testdata/%s.json", tc.filename))
-			actual, err := Convert(input, nil)
+			actual, err := Convert(input, tc.opts)
 			if tc.expectErr {
 				if err == nil {
 					t.Fatalf("expected error, got nil")
